cf9c25bf5bfc6eef161b9ff55445c60f641fca79
Daniel Garofalo <danielgarofalo@Daniels-MacBook-Pro-2.local>
2019-01-17 18:16:13 -0600

    Started Web request for register user. To be finished

---
+++ SmackApp/Services/AuthService.swift
@@ -0,0 +1,99 @@
+//
+//  AuthService.swift
+//  SmackApp
+//
+//  Created by Daniel Garofalo on 1/17/19.
+//  Copyright © 2019 Daniel Garofalo. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    //MARK:-  Singleton
+    static let instance = AuthService()
+    
+    //MARK:-  Properties
+    //To save state of the session
+    let defaults = UserDefaults.standard
+    
+    //State of the session. Values to be used and passed on each REST call
+    var isLoggedIn : Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    //Token for log in
+    var authToken : String {
+        get{
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    //Email used on the session
+    var userEmail : String {
+        get{
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    
+    
+    //MARK:-  Authorization Calls. In order:
+    //1.Register User
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler){
+        
+        let lowercasedEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowercasedEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .get, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            if response.result.error == nil {
+                
+                
+                
+                
+                
+                completion(true)
+            } else {
+                
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+    
+    
+    
+    //2.Login User
+    
+    //3.Add user
+    
+    
+    
+    
+    
+    
+    
+}
+
--- SmackApp/Utilities/Constants.swift
+++ SmackApp/Utilities/Constants.swift
@@ -8,9 +8,24 @@
 
 import Foundation
 
+//Completion Handler
+typealias CompletionHandler = (_ Success: Bool) -> () //Closure to determine if call passed or not
+
+//URLS
+let BASE_URL = "http://localhost:3005/v1"
+let URL_REGISTER = "\(BASE_URL)/account/register"
+
+
+
+
 //Segues
 let GO_TO_LOGIN = "goToLogin"
 let GO_TO_CREATE_ACCOUNT = "goToCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
 
 
